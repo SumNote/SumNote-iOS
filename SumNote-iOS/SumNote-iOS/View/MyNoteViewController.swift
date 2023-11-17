cb16f2faf0212aeb8310f09670255ca24f855c37
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-11-17 22:54:22 +0900

    FEAT: Get User Kakao Information from KaKaoAPI

--- SumNote-iOS/SumNote-iOS/View/MyNoteViewController.swift
+++ SumNote-iOS/SumNote-iOS/View/MyNoteViewController.swift
@@ -7,12 +7,37 @@
 
 import UIKit
 
+import KakaoSDKUser
+import Kingfisher
+
 class MyNoteViewController: UIViewController {
 
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var userImageLabel: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        userImageLabel.layer.cornerRadius = 25
+        setUserInfo()
+    }
+    
+    private func setUserInfo(){
+        UserApi.shared.me() {(user, error) in
+            if let error = error {
+                print(error)
+            } else if let userInfo = KaKaoAPI.shared.extractUserInfo(user) {
+                print("### Extracted User Info: \(userInfo)")
+                self.userNameLabel.text = userInfo["nickname"] as? String
+                let urlString = userInfo["profileImage"]
+                let url = URL(string: urlString as! String)
+                self.userImageLabel.kf.setImage(with:url)
+                
+                self.userEmailLabel.text = userInfo["email"] as? String
+            }
+        }
     }
     
 }

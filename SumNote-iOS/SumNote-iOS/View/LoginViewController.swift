5c2b69b015906c32cce8a0802f0589aaa2477142
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-11-17 22:05:45 +0900

    ADD LoginView

--- SumNote-iOS/SumNote-iOS/View/LoginViewController.swift
+++ SumNote-iOS/SumNote-iOS/View/LoginViewController.swift
@@ -12,11 +12,23 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var btnKaKaoLogin: UIButton! //카카오 로그인 버튼
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        btnKaKaoLogin.layer.cornerRadius = 20 //테두리 둥글게
     }
+    
+    
 
-
+    // 카카오 계정으로 시작하기 버튼 클릭시
+    @IBAction func kakaoLoginBtnDidTapped(_ sender: Any) {
+//        // 1. 스토리보드 찾기
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        // 2. 이동할 뷰 찾기 => 스토리보드의 identifier릁 통해
+//        let MainView = storyboard.instantiateViewController(identifier: "MainViewController")
+    }
+    
 }
 

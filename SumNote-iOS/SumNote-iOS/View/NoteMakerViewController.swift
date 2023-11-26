4884c30c4d1cca852a4d9a536a0dc8503f34514a
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-11-26 12:36:54 +0900

    노트 메이커 뷰 작업 완료

--- SumNote-iOS/SumNote-iOS/View/NoteMakerViewController.swift
+++ SumNote-iOS/SumNote-iOS/View/NoteMakerViewController.swift
@@ -11,9 +11,30 @@ import UIKit
 // 사용자가 찍은 사진을 서버로 전송하여 노트 생성 화면으로 이동
 class NoteMakerViewController: UIViewController {
 
+    //MARK: layout
+    @IBOutlet weak var btnCamera: UIButton!
+    @IBOutlet weak var btnGallery: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setBtnLayout()
+    }
+    
+    // 버튼 테두리 둥글게 적용
+    private func setBtnLayout(){
+        self.btnCamera.layer.cornerRadius = 25
+        self.btnGallery.layer.cornerRadius = 25
+    }
+    
+    //MARK: Camera Button Action
+    @IBAction func btnCameraDidTapped(_ sender: Any) {
+        
+    }
+    
+    //MARK: Gallery Button Action
+    @IBAction func btnGalleryDidTapped(_ sender: Any) {
+        
     }
 }

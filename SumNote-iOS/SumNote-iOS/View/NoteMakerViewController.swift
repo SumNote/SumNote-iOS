c8ac8a88a1ec38e2553eb1457b9992f9ef4d8994
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-11-26 13:03:16 +0900

    노트 메이커 : 카메라,갤러리 사용 이벤트 추가

--- SumNote-iOS/SumNote-iOS/View/NoteMakerViewController.swift
+++ SumNote-iOS/SumNote-iOS/View/NoteMakerViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-
 // 사용자가 찍은 사진을 서버로 전송하여 노트 생성 화면으로 이동
 class NoteMakerViewController: UIViewController {
 
@@ -15,11 +14,12 @@ class NoteMakerViewController: UIViewController {
     @IBOutlet weak var btnCamera: UIButton!
     @IBOutlet weak var btnGallery: UIButton!
     
+    private let imagePickerViewController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        setBtnLayout()
+        setBtnLayout() // 버튼 테두리 둥글게 적용
+        imagePickerViewController.delegate = self
     }
     
     // 버튼 테두리 둥글게 적용
@@ -30,11 +30,35 @@ class NoteMakerViewController: UIViewController {
     
     //MARK: Camera Button Action
     @IBAction func btnCameraDidTapped(_ sender: Any) {
-        
+        // imagePicker의 타입 결정
+        // 1. photoLibrary : 갤러리
+        // 2. camera : 시스템 카메라
+        self.imagePickerViewController.sourceType = .camera // 카메라로 선택
+        self.present(imagePickerViewController,animated: true,completion: nil)
     }
     
     //MARK: Gallery Button Action
     @IBAction func btnGalleryDidTapped(_ sender: Any) {
-        
+        //1. imagePicker의 타입 선택
+        self.imagePickerViewController.sourceType = .photoLibrary
+        //2. NavigationController를 사용하여, ImagePickerContoller로 화면 전환
+        // completion은 이후에 수행할 함수를 의미?
+        self.present(imagePickerViewController,animated: true,completion: nil)
     }
+}
+
+// 뷰를 전환하기 위한 NavigationController와, 카메라,갤러리 뷰를 사용하기 위해 ImagePickerControllerDelegate 프로토콜을 채택한다.
+extension NoteMakerViewController : UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    // 이미지가 선택된 후에, 아래 함수가 호출된다.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            print(image) // 사용자가 이미지를 선택하거나, 촬영하면 동작함
+        }
+        // 사용자가 선택한 이미지를 Multipart를 사용하여 서버로 전송하는 로직 작성 필요
+        // OCR + GPT Generation이 진행중인 동안 indicator를 사용하여 화면에 로딩 띄우는 작업 필요
+        // 로딩이 끝난 이후 노트 페이지로 이동 하는 작업 필요(네비게이션으로?)
+    }
+    
+    
 }

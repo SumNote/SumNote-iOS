a9f186890de4b773fbee88bf6a76387ac71714bc
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-03 21:56:37 +0900

    ADD : AllNoteViewController

---
+++ SumNote-iOS/SumNote-iOS/View/AllNoteViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AllNoteViewController.swift
+//  SumNote-iOS
+//
+//  Created by Choi76 on 2023/12/03.
+//
+
+import UIKit
+
+
+// 사용자가 보유중인 모든 노트 목록을 보여줌
+class AllNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}

102565cd76581fb0b0e4227f071c863563ff15fc
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-04 18:25:59 +0900

    ADD : All-Quiz-CollectionView-Cell

---
+++ SumNote-iOS/SumNote-iOS/Cell/AllQuizCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  AllQuizCollectionViewCell.swift
+//  SumNote-iOS
+//
+//  Created by Choi76 on 2023/12/04.
+//
+
+import UIKit
+
+class AllQuizCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

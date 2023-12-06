16e43969a8bc2e9b9d517d29d9e1349009dfdd34
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-07 00:40:21 +0900

    ADD : AllQuizTableViewCell Code

---
+++ SumNote-iOS/SumNote-iOS/Cell/AllQuizTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  AllQuizTableViewCell.swift
+//  SumNote-iOS
+//
+//  Created by Choi76 on 2023/12/07.
+//
+
+import UIKit
+
+class AllQuizTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var quizImage: UIImageView!
+    @IBOutlet weak var quizGenDate: UILabel!
+    @IBOutlet weak var quizTitle: UILabel!
+    
+    static let identifier = "AllQuizTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        quizImage.layer.cornerRadius = 10
+    }
+
+    
+    // 선택시 동작
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- SumNote-iOS/SumNote-iOS/View/AllQuizViewController.swift
+++ SumNote-iOS/SumNote-iOS/View/AllQuizViewController.swift
@@ -9,10 +9,36 @@ import UIKit
 
 class AllQuizViewController: UIViewController {
 
+    @IBOutlet weak var allQuizTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        setTableView()
+        
+    }
+    
+    private func setTableView(){
+        allQuizTableView.delegate = self
+        allQuizTableView.dataSource = self
+        
+        //nib 등록
     }
 
+}
+
+extension AllQuizViewController : UITableViewDelegate,UITableViewDataSource{
+    // 몇개의 데이터를 보여줄 것인지
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 보여줄 셀의 모습 지정
+        let errorCell = UITableViewCell()
+        errorCell.backgroundColor = .blue
+        return errorCell
+    }
+    
+    
 }

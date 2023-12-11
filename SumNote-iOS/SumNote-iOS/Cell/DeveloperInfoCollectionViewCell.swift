ac58e322f518bdece3f91d5a97f7864a51e047dc
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-11 16:10:53 +0900

    ADD : 개발자 셀

---
+++ SumNote-iOS/SumNote-iOS/Cell/DeveloperInfoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DeveloperInfoCollectionViewCell.swift
+//  SumNote-iOS
+//
+//  Created by Choi76 on 2023/12/11.
+//
+
+import UIKit
+
+class DeveloperInfoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- SumNote-iOS/SumNote-iOS/View/UserInfoViewController.swift
+++ SumNote-iOS/SumNote-iOS/View/UserInfoViewController.swift
@@ -7,12 +7,40 @@
 
 import UIKit
 
+// 개발자 정보 페이지
 class UserInfoViewController: UIViewController {
 
+    @IBOutlet weak var developerInfoCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    func setCollectionView(){
+        developerInfoCollectionView.delegate = self
+        developerInfoCollectionView.dataSource = self
+        
+        // 사용할 셀 등록
+    }
+    
+}
+
+// collectionview 프로토콜 채택
+extension UserInfoViewController : UICollectionViewDelegate,UICollectionViewDataSource{
+    
+    // 메인 화면, 강민서, 김태하, 최강, 이경민
+    // 5개의 셀 사용 예정
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    // 셀 등록
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
     
 }

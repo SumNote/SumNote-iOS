3a9b199d98ec8656977d446bb078978f64d42190
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-13 16:40:03 +0900

    FEAT : UIPageViewController - viewControllerBefore&After

--- SumNote-iOS/SumNote-iOS/View/NotePageViewController.swift
+++ SumNote-iOS/SumNote-iOS/View/NotePageViewController.swift
@@ -12,9 +12,9 @@ import UIKit
 // 좌우 슬라이드를 통해 노트의 내용을 보며 학습 가능
 class NotePageViewController: UIPageViewController {
 
-    // 페이지 뷰를 통해 넘겨줄 화면들을에 대한 배열
+    // 페이지 뷰를 통해 넘겨줄 화면들에 대한 배열
     private var pages = [UIViewController]()
-    // 시작 페이지 번호
+    // 시작 페이지 번호 => 화면 이동시 사용
     private var initialPage = 0
     
     override func viewDidLoad() {
@@ -25,7 +25,32 @@ class NotePageViewController: UIPageViewController {
     
     // 페이지에 값 할당
     private func setupPages(){
-        //
+        
     }
 
+}
+
+// MARK: - DataSource
+
+extension NotePageViewController: UIPageViewControllerDataSource {
+    
+    // 우측 -> 좌측으로 슬라이드 제스처 발생시
+    // 이전 뷰 컨트롤러를 리턴
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        // 현재 ViewController의 인덱스 구하기
+        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
+        guard currentIndex > 0 else { return nil }
+        return pages[currentIndex - 1] // 이전 인덱스의 ViewController 리턴
+    }
+    
+    // 좌측 - 우측으로 슬라이드 제스처 발생시
+    // 다음 뷰 컨트롤러를 리턴
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        // 현재 ViewController의 인덱스 구하기
+        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
+        guard currentIndex < (pages.count - 1) else { return nil }
+        return pages[currentIndex + 1]
+    }
+    
+    
 }

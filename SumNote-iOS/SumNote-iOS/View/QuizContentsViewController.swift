0a6b142f1630b1c50b29679bcab2a190906be3a4
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-14 23:35:04 +0900

    ADD : QuizPageVC & QuizContentVC

---
+++ SumNote-iOS/SumNote-iOS/View/QuizContentsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuizContentsViewController.swift
+//  SumNote-iOS
+//
+//  Created by Choi76 on 2023/12/14.
+//
+
+import UIKit
+
+class QuizContentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SumNote-iOS/SumNote-iOS/View/QuizPageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  QuizPageViewController.swift
+//  SumNote-iOS
+//
+//  Created by Choi76 on 2023/12/14.
+//
+
+import UIKit
+
+
+// 사용자가 퀴즈를 풀 수 있도록
+class QuizPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

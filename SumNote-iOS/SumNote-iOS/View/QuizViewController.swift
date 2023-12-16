be8ae5076aaed4903e852185d91d24f3079b138f
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-16 14:06:31 +0900

    ADD : QuizViewController Layout

---
+++ SumNote-iOS/SumNote-iOS/View/QuizViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QuizViewController.swift
+//  SumNote-iOS
+//
+//  Created by Choi76 on 2023/12/16.
+//
+
+import UIKit
+
+// 퀴즈페이지뷰를 포함할 퀴즈뷰
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}

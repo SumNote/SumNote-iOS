80b2b1d2d9bdb76d03ea022de6d2a9cc36744131
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-24 12:25:09 +0900

    FEAT : 전체 노트 네비게이션 바 커스텀

--- SumNote-iOS/SumNote-iOS/View/AllQuizViewController.swift
+++ SumNote-iOS/SumNote-iOS/View/AllQuizViewController.swift
@@ -13,11 +13,44 @@ class AllQuizViewController: UIViewController {
     
     weak var delegater : NavigationDelegate? // 화면 이동을 위한 위임자 지정
     
+    @IBOutlet weak var backBtn: UIImageView! // 뒤로가기 버튼(이미지)
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setTableView()
         
+        // 뒤로가기 이벤트 정의
+        // 탭 제스처 인식기 설정 => 뒤로가기 버튼 사용을 위해
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(backBtnTapped))
+        backBtn.isUserInteractionEnabled = true // 사용자 상호작용 활성화
+        backBtn.addGestureRecognizer(tapGesture)
+    }
+    
+    // 뷰가 실행되고 난 이후 (네비게이션 바 커스텀을 위해 상단 바 숨기기)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 네비게이션 바 숨기기
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    // 뷰가 사라질 때 (네비게이션 바 다시 보일 수 있도록)
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // 다른 뷰로 이동할 때 네비게이션 바를 다시 보이게 설정
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    
+    // MARK: functions
+    // 백 버튼 탭 처리
+    // 네비게이션 컨트롤러에서 현재 뷰 컨트롤러 제거
+    @objc func backBtnTapped() {
+        self.navigationController?.popViewController(animated: true)
+        print("뒤로가기 클릭됨")
     }
     
     private func setTableView(){
@@ -27,6 +60,8 @@ class AllQuizViewController: UIViewController {
         //nib 등록
         allQuizTableView.register(UINib(nibName: "AllQuizTableViewCell", bundle: nil), forCellReuseIdentifier: AllQuizTableViewCell.identifier)
     }
+    
+    
 
 }
 

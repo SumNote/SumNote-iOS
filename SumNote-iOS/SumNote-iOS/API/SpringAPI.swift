ab96f1d549f9be39dbd476d7c8464936b983ee21
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-29 23:48:25 +0900

    ADD : SpringAPI - 사용자의 노트&퀴즈 데이터 요청용

---
+++ SumNote-iOS/SumNote-iOS/API/SpringAPI.swift
@@ -0,0 +1,31 @@
+//
+//  SpringAPI.swift
+//  SumNote-iOS
+//
+//  Created by Choi76 on 2023/12/29.
+//
+
+import Foundation
+import Alamofire
+
+class SpringAPI{
+    
+    static let shared = SpringAPI() // Singleton
+    
+    private init() {} // for singleton
+    
+    // 기본적인 CRUD 작업
+    
+    // 사용자의 노트 데이터 요청(노트 목록)
+    
+    // 사용자의 특정 노트의 페이지 데이터 요청(페이지들)
+    
+    // 사용자의 문제집 데이터 요청(문제집 목록)
+    
+    // 사용자의 특정 문제집의 퀴즈 데이터 요청(문제집에 소속된 퀴즈들)
+    
+    // 사용자의 노트 삭제 요청
+    
+    // 사용자의 퀴즈 삭제 요청
+    
+}

93f504c62e29ff30b91cb2b10d6da746abda5e22
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-03-21 16:36:51 +0900

    ADD : CreatedNoteViewController - 생성된 노트 확인 뷰

---
+++ SumNote-iOS/SumNote-iOS/view/CreatedNoteViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CreatedNoteViewController.swift
+//  SumNote-iOS
+//
+//  Created by Choi76 on 2024/03/21.
+//
+
+import UIKit
+
+// 생성된 노트에 대한 미리보기
+// 사용자는 생성된 노트를 확인하고 원한다면 저장, 원하지 않는다면 되돌아간다.
+class CreatedNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

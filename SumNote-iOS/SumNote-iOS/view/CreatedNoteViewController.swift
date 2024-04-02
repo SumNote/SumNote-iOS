3f976b4f48f1f6132ddecc4bfb97eda99419085f
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-04-02 20:33:48 +0900

    feat : 취소 버튼 클릭시 뒤로가기

--- SumNote-iOS/SumNote-iOS/view/CreatedNoteViewController.swift
+++ SumNote-iOS/SumNote-iOS/view/CreatedNoteViewController.swift
@@ -17,7 +17,27 @@ class CreatedNoteViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 네비게이션 바 숨기기
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // 다른 뷰로 이동할 때 네비게이션 바를 다시 보이게 설정
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    
+    // 취소 => 되돌아가기
+    @IBAction func cancelBtnDidTapped(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+        print("CreatedNoteViewController : cancelBtnDidTapped")
+    }
+    
     /*
     // MARK: - Navigation
 

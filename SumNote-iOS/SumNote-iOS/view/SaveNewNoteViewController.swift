0933660b6037fea16430bbcff838bd207654eeec
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-05-06 15:23:09 +0900

    ✨ SaveNoteViewController 키보드 인터럽트 작성
    키보드가 올라올때 다이얼로그의 오토레이아웃 동적으로 조정

--- SumNote-iOS/SumNote-iOS/view/SaveNewNoteViewController.swift
+++ SumNote-iOS/SumNote-iOS/view/SaveNewNoteViewController.swift
@@ -18,13 +18,48 @@ class SaveNewNoteViewController: UIViewController {
     var noteContent : String?
     weak var finishNoteSaveDelegate: FinishNoteSaveDelegate?
     
+    @IBOutlet weak var dialogCenterYConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let doneBtnTapGesture = UITapGestureRecognizer(target: self, action: #selector(saveByNewNote))
         doneBtnView.addGestureRecognizer(doneBtnTapGesture)
         
+        // 키보드 동작시, 뷰의 높이가 높아져야함(동적 오토레이아웃)
+        newNoteTitle.addTarget(self, action: #selector(startEditing), for: .editingDidBegin)
+        newNoteTitle.addTarget(self, action: #selector(endEditing), for: .editingDidEnd)
+    
+        
+        // 뷰 전체에 대한 탭 제스처 인식기 추가
+        let keyBoardTapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        keyBoardTapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(keyBoardTapGesture)
+        
     }
+    
+    // 키보드가 나타나면 다이얼로그를 위로 이동
+    @objc func startEditing(sender : UITextField){
+        dialogCenterYConstraint.constant -= 50
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    // 키보드가 사라지면 다이얼로그를 아래로 이동
+    @objc func endEditing(sender : UITextField){
+        dialogCenterYConstraint.constant += 50
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    // 다른 영역 클릭시 키보드 종료
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
     
     @objc private func saveByNewNote(_ sender: UITapGestureRecognizer){
         

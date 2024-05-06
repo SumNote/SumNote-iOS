71ba46ad108072b2de3b32c2ea907fc65ae401d7
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-05-06 15:26:24 +0900

    ✨ ChangeNoteTitleViewController 키보드 인터럽트 추가
    키보드가 올라올 때, 동적으로 오토레이아웃 조정

--- SumNote-iOS/SumNote-iOS/view/ChangeNoteTitleViewController.swift
+++ SumNote-iOS/SumNote-iOS/view/ChangeNoteTitleViewController.swift
@@ -12,6 +12,10 @@ class ChangeNoteTitleViewController: UIViewController {
     @IBOutlet weak var changeNoteTitleEditText: UITextField!
     @IBOutlet weak var changeNoteTitleViewBtn: UIView!
     
+    
+    @IBOutlet weak var dialogCenterYConstraint: NSLayoutConstraint!
+    
+    
     var noteId : Int?
     weak var changeNotTitleDelegate : ChangeNoteTitleDelegate?
     
@@ -20,7 +24,41 @@ class ChangeNoteTitleViewController: UIViewController {
         self.log("viewDidLoad noteId : \(String(describing: noteId))")
         let changeNoteTitleGesture = UITapGestureRecognizer(target: self, action: #selector(changeNoteTitle))
         changeNoteTitleViewBtn.addGestureRecognizer(changeNoteTitleGesture)
+        
+        // 키보드 동작시, 뷰의 높이가 높아져야함(동적 오토레이아웃)
+        changeNoteTitleEditText.addTarget(self, action: #selector(startEditing), for: .editingDidBegin)
+        changeNoteTitleEditText.addTarget(self, action: #selector(endEditing), for: .editingDidEnd)
+    
+        // 뷰 전체에 대한 탭 제스처 인식기 추가
+        let keyBoardTapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        keyBoardTapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(keyBoardTapGesture)
+        
+        
     }
+    
+    // 키보드가 나타나면 다이얼로그를 위로 이동
+    @objc func startEditing(sender : UITextField){
+        dialogCenterYConstraint.constant -= 50
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    // 키보드가 사라지면 다이얼로그를 아래로 이동
+    @objc func endEditing(sender : UITextField){
+        dialogCenterYConstraint.constant += 50
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    // 다른 영역 클릭시 키보드 종료
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
     
     @objc func changeNoteTitle(){
         let newTitle = self.changeNoteTitleEditText.text
